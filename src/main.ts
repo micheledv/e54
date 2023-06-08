794456fee57531a6bdc8da6e9dfab5313ada2df1
Michele Delle Vergini <michele.dellevergini@outlook.com>
2023-06-08 17:46:37 +0100

    Make sure the main handler returns void

--- src/main.ts
+++ src/main.ts
@@ -24,13 +24,17 @@ const repo = LocalRepo.load('quotes.yaml')
 const smartAddService = new SmartAddCollector(20)
 
 const handler = (bindings: BotServiceBinding[]) => {
-  return async (message: Message) => {
+  const fn = async (message: Message) => {
     console.log(message)
 
     const anyBindingMatched = await runBindings(bindings, message)
     if (anyBindingMatched) return
 
     smartAddService.trackMessage(message)
+  }
+
+  return (message: Message) => {
+    fn(message).catch(console.error)
   }
 }
 
